b224c8365ea3e79972607e73671eaa32fc3cefac
Demo <fozirawat6869@gmail.com>
2026-09-17 00:01:24 +0530

    Handle case when no numbers are missing

--- Arrays/medium/findTheMissingNoInAnArray.js
+++ Arrays/medium/findTheMissingNoInAnArray.js
@@ -1,28 +1,34 @@
 function missingNumbers(arr) {
-    let n = arr[0];
+  let n = arr[0];
+  let missingFound = false;
 
-    // Find largest number
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > n) {
-            n = arr[i];
-        }
+  // Find largest number
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > n) {
+      n = arr[i];
+    }
+  }
+
+  // Find missing numbers
+  for (let i = arr[0]; i <= n; i++) {
+    let found = false;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === i) {
+        found = true;
+        break;
+      }
     }
 
-    // Find missing numbers
-    for (let i = arr[0]; i <= n; i++) {
-        let found = false;
-
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j] === i) {
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) {
-            console.log(i);
-        }
+    if (!found) {
+      console.log("Missing:", i);
+      missingFound = true;
     }
+  }
+
+  if (!missingFound) {
+    console.log("No missing numbers");
+  }
 }
 
-missingNumbers([2, 5, 9]);
+missingNumbers([3, 4, 5, 6, 7, 10]);
